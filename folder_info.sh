a68f858dd56c49251c555ee62c588fe458885abc
lpmor22 <laisepaixao@live.com>
2021-05-31 05:47:51 -0300

    Update folder_info.sh

--- folder_info.sh
+++ folder_info.sh
@@ -2,6 +2,8 @@
 
 DATE="$(date +'%Y-%m-%d')"
 
+[ ! -d "$ANALYSISDIR" ] && mkdir "$ANALYSISDIR" -v
+
 echo "folder@read" | tr '@' '\t' > $HOME/IAM_SARSCOV2/ANALYSIS/"$DATE"_folder_info.txt
 
 find ./ -type f -name "*.fastq.gz" | awk -F'/' '{print $(NF-1),$NF}' | tr '[:blank:]' '\t' | awk '/.fastq.gz/d' >> $HOME/IAM_SARSCOV2/ANALYSIS/"$DATE"_folder_info.txt
