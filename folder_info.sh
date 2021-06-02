c2c320e58e012efd6505f729aec630817eade323
lpmor22 <laisepaixao@live.com>
2021-06-02 18:54:20 -0300

    Update folder_info.sh

--- folder_info.sh
+++ folder_info.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-DATE="$(date +'%Y-%m-%d')"
 ANALYSISDIR="$HOME/IAM_SARSCOV2/ANALYSIS" # analysis path directory
 
 [ ! -d "$ANALYSISDIR" ] && mkdir "$ANALYSISDIR"
 
-echo "folder@read" | tr '@' '\t' > $HOME/IAM_SARSCOV2/ANALYSIS/"$DATE"_folder_info.txt
+chmod 700 -R "$ANALYSISDIR"
 
-find ./ -type f -name "*.fastq.gz" | awk -F'/' '{print $(NF-1),$NF}' | tr '[:blank:]' '\t' | awk '/.fastq.gz/d' >> $HOME/IAM_SARSCOV2/ANALYSIS/"$DATE"_folder_info.txt
+echo "folder@read" | tr '@' '\t' > "$ANALYSISDIR"/"$(echo $PWD | awk -F'/' '{print $NF}')_folder_info.txt"
+
+find ./ -type f -name "*.fastq.gz" | awk -F'/' '{print $(NF-1),$NF}' | tr '[:blank:]' '\t' | awk '/.fastq.gz/d' >> "$ANALYSISDIR"/"$(echo $PWD | awk -F'/' '{print $NF}')_folder_info.txt"
