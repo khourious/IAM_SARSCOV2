b712f2907ec069bf8f2f5bf9498b0240d6286bc4
lpmor22 <laisepaixao@live.com>
2021-06-02 18:54:49 -0300

    Update iam_sarscov2.sh

--- iam_sarscov2.sh
+++ iam_sarscov2.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
 
-start=$(date +%s.%N)
+function background {
 
-DATE="$(date +'%Y-%m-%d')"
-ANALYSISDIR="$HOME/IAM_SARSCOV2/ANALYSIS" # analysis path directory
-THREADS="$(lscpu | grep 'CPU(s):' | awk '{print $2}' | sed -n '1p')"
-DEPTH="10" # minimum depth to mask unanssembled regions
-MINLEN="75" # minimum length to trimm sequences
+    start=$(date +%s.%N)
 
-[ ! -d "$ANALYSISDIR" ] && mkdir "$ANALYSISDIR" -v
+    ANALYSISDIR="$HOME/IAM_SARSCOV2/ANALYSIS" # analysis path directory
 
-for i in $(find ./ -type f -name "*R1*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d- -f1)"_R1.fastq.gz -v; done
-for i in $(find ./ -type f -name "*R2*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d- -f1)"_R2.fastq.gz -v; done
-# for i in $(find ./ -type f -name "*R1*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d_ -f1)"_R1.fastq.gz -v; done
-# for i in $(find ./ -type f -name "*R2*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d_ -f1)"_R2.fastq.gz -v; done
+    DEPTH="10" # minimum depth to mask unanssembled regions
 
-cd "$ANALYSISDIR"
+    MINLEN="75" # minimum length to trimm sequences
 
-for i in $(find ./ -type f -name "*.fastq.gz" | while read o; do basename $o; done | cut -d_ -f1 | sort |uniq); do bash $HOME/IAM_SARSCOV2/sars2_assembly.sh $HOME/IAM_SARSCOV2/nCoV-2019.reference.fasta "$i"_R1.fastq.gz "$i"_R2.fastq.gz "$i" "$THREADS" "$DEPTH" "$MINLEN" $HOME/IAM_SARSCOV2/nCoV-2019.primers.fasta; done
+    RAWDIR=$PWD
 
-for i in $(find ./ -type f -name "*chromosomes.report"); do cat "$i"; done | (sed -u 1q; sort) | uniq | sed '$d' > "$DATE"_stats.txt
-for i in $(find ./ -type f -name "*.depth10.minor.fa"); do cat "$i" >> "$DATE"_consensus.tmp; done
-for i in $(find ./ -type f -name "*.depth10.fa"); do cat "$i" >> "$DATE"_consensus.tmp; done
-seqkit grep -vip "MN908947.3_minor" "$DATE"_consensus.tmp | seqkit sort -n - > "$DATE"_consensus.fa
+    THREADS="$(lscpu | grep 'CPU(s):' | awk '{print $2}' | sed -n '1p')"
 
-source activate plot
-for i in $(find ./ -type f -name "*.bam" | while read o; do basename $o | cut -d. -f1; done | sort | uniq); do fastcov -l "$i".results/"$i".sorted.bam -o "$i".results/"$i".coverage.pdf; gs -dSAFER -r3000 -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sOUTPUTFILE="$DATE"_coverage_depth.pdf "$i".results/*.pdf; done
+    [ ! -d "$ANALYSISDIR" ] && mkdir "$ANALYSISDIR" -v
 
-rm -rf *.fastq.gz *.tmp
+    chmod 700 -R "$ANALYSISDIR"
 
-end=$(date +%s.%N)
+    for i in $(find ./ -type f -name "*R1*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d_ -f1)"_R1.fastq.gz -v; done
+    # for i in $(find ./ -type f -name "*R1*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d- -f1)"_R1.fastq.gz -v; done
 
-runtime=$(python -c "print(${end} - ${start})")
+    for i in $(find ./ -type f -name "*R2*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d_ -f1)"_R2.fastq.gz -v; done
+    # for i in $(find ./ -type f -name "*R2*"); do cp "$i" "$ANALYSISDIR"/"$(echo "$i" | awk -F'/' '{print $(NF-1)}' | cut -d- -f1)"_R2.fastq.gz -v; done
 
-echo "" && echo "Done. The runtime was $runtime seconds."
+    for i in $(find "$ANALYSISDIR" -type f -name "*.fastq.gz" | while read o; do basename $o; done | cut -d_ -f1 | sort |uniq); do bash $HOME/IAM_SARSCOV2/sars2_assembly.sh $HOME/IAM_SARSCOV2/nCoV-2019.reference.fasta "$ANALYSISDIR"/"$i"_R1.fastq.gz "$ANALYSISDIR"/"$i"_R2.fastq.gz "$i" "$THREADS" "$DEPTH" "$MINLEN" $HOME/IAM_SARSCOV2/nCoV-2019.primers.fasta "$ANALYSISDIR"; done
+
+    for i in $(find "$ANALYSISDIR" -type f -name "*.fastq.gz" | while read o; do basename $o; done | cut -d_ -f1 | sort |uniq); do cat "$ANALYSISDIR"/*.results/chromosomes.report | (sed -u 1q; sort) | uniq | sed '$d' > "$(pwd | awk -F/ '{print $NF}')_stats.txt"; done
+
+    for i in $(find "$ANALYSISDIR" -type f -name "*.fastq.gz" | while read o; do basename $o; done | cut -d_ -f1 | sort |uniq); do cat "$ANALYSISDIR"/"$i".results/"$i".depth10.minor.fa >> "$(pwd | awk -F/ '{print $NF}')_consensus.tmp"; done
+
+    for i in $(find "$ANALYSISDIR" -type f -name "*.fastq.gz" | while read o; do basename $o; done | cut -d_ -f1 | sort |uniq); do cat "$ANALYSISDIR"/"$i".results/"$i".depth10.fa >> "$(pwd | awk -F/ '{print $NF}')_consensus.tmp"; done
+
+    source activate iam_sarscov2
+
+    seqkit grep -vip "MN908947.3_minor" "$(pwd | awk -F/ '{print $NF}')_consensus.tmp" | seqkit sort -n > "$(pwd | awk -F/ '{print $NF}')_consensus.fa"
+
+    source activate plot
+
+    for i in $(find "$ANALYSISDIR" -type f -name "*.fastq.gz" | while read o; do basename $o; done | cut -d_ -f1 | sort |uniq); do fastcov -l "$ANALYSISDIR"/"$i".results/"$i".sorted.bam -o "$ANALYSISDIR"/"$i".results/"$i".coverage.pdf; gs -dSAFER -r3000 -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sOUTPUTFILE="$(pwd | awk -F/ '{print $NF}')_coverage_depth.pdf" "$ANALYSISDIR"/*.results/*.pdf; done
+
+    mv $(pwd | awk -F/ '{print $NF}')* "$ANALYSISDIR"
+
+    rm -rf "$ANALYSISDIR"/*.fastq.gz "$ANALYSISDIR"/*.tmp
+
+    end=$(date +%s.%N)
+
+    runtime=$(python -c "print(${end} - ${start})")
+
+    echo "" && echo "Done. The runtime was $runtime seconds."
+
+}
+
+export -f background
+
+nohup bash -c background > "$(pwd | awk -F/ '{print $NF}')_log.txt" &
+
