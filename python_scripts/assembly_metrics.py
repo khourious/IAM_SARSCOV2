412fa033f650f50033631b68f7e5d0c408f46abd
Laise de Moraes <laisepaixao@live.com>
2021-07-08 15:10:20 -0300

    change chromosomes.report info

--- python_scripts/assembly_metrics.py
+++ python_scripts/assembly_metrics.py
@@ -9,12 +9,11 @@ prefixout = args.prefixout
 
 try:
     os.system(f"bedtools bamtobed -i {prefixout}.sorted.bam > {prefixout}.sorted.bed \
-                    && samtools view {prefixout}.sorted.bam -u | bamdst -p {prefixout}.sorted.bed -o . \
+                    && samtools view {prefixout}.sorted.bam -u | bamdst -p {prefixout}.sorted.bed --cutoffdepth 1000 -o . \
                     && gunzip ./region.tsv.gz \
                     && gunzip ./depth.tsv.gz \
-                    && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' chromosomes.report \
-                    && sed -i -e 's/MN908947\.3/'{prefixout}'/g' chromosomes.report \
-                    && sed -i -e 's/#Chromosome/Sample ID/g' chromosomes.report \
-                    && sed -i -e 's/__/\//g' -e 's/--/|/g' chromosomes.report")
+                    && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' -e 's/MN908947\.3/'{prefixout}'/g' -e 's/#Chromosome/Sample_ID/g' -e 's/__/\//g' -e 's/--/|/g' -e 's/Avg depth/Avg_depth/g' -e 's/Cov /Cov_/g' -e 's/%//g' chromosomes.report \
+                    $$ awk '{$1=$1} 1' OFS="\t" chromosomes.report \
+                    $$ awk -F"\t" '{print $1,$3,$7,$9,$10}' chromosomes.report")
 except:
     print("Error in assembly metrics step")
