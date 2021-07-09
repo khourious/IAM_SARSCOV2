09b514e140e9418febeb147e5c3b496a0dbea7f3
Laise de Moraes <laisepaixao@live.com>
2021-07-09 17:39:05 -0300

    Update assembly_metrics.py

--- python_scripts/assembly_metrics.py
+++ python_scripts/assembly_metrics.py
@@ -13,7 +13,7 @@ try:
                     && gunzip ./region.tsv.gz \
                     && gunzip ./depth.tsv.gz \
                     && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' chromosomes.report \
+                    && sed -i -e 's/MN908947\.3/'{prefixout}'/g' chromosomes.report \
                     && sed -i -e 's/__/\//g' -e 's/--/|/g' chromosomes.report")
-                    && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' -e 's/MN908947\.3/'{prefixout}'/g' -e 's/#Chromosome/SampleId/g' -e 's/__/\//g' -e 's/--/|/g' -e 's/Avg depth/AverageDepth/g' -e 's/Cov /Coverage/g' -e 's/%//g' chromosomes.report")
 except:
     print("Error in assembly metrics step")
