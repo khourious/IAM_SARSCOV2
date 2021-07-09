63a4dfc69e9da51a86dcbe03638a527bcba238d1
Laise de Moraes <laisepaixao@live.com>
2021-07-09 14:58:10 -0300

    Update assembly_metrics.py

--- python_scripts/assembly_metrics.py
+++ python_scripts/assembly_metrics.py
@@ -9,12 +9,11 @@ prefixout = args.prefixout
 
 try:
     os.system(f"bedtools bamtobed -i {prefixout}.sorted.bam > {prefixout}.sorted.bed \
-                    && samtools view {prefixout}.sorted.bam -u | bamdst -p {prefixout}.sorted.bed --cutoffdepth 1000 -o . \
+                    && samtools view {prefixout}.sorted.bam -u | bamdst --cutoffdepth 1000 -p {prefixout}.sorted.bed -o . \
                     && gunzip ./region.tsv.gz \
                     && gunzip ./depth.tsv.gz \
-                    && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' -e 's/MN908947\.3/'{prefixout}'/g' -e 's/#Chromosome/Sample_ID/g' -e 's/__/\//g' -e 's/--/|/g' -e 's/Avg depth/Avg_depth/g' -e 's/Cov /Cov_/g' -e 's/%//g' chromosomes.report \
-                    && awk '{$1=$1} 1' OFS=, chromosomes.report > chromosomes.report.tmp1 \
-                    && awk -F, '{print $1,$3,$7,$9,$10}' chromosomes.report.tmp1 > chromosomes.report.tmp2 \
-                    && cat chromosomes.report.tmp2 | tr ' ' '\t' > chromosomes.report")
+                    && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' chromosomes.report \
+                    && sed -i -e 's/__/\//g' -e 's/--/|/g' chromosomes.report")
+                    && sed -i -e 's/NC_045512\.2/'{prefixout}'/g' -e 's/MN908947\.3/'{prefixout}'/g' -e 's/#Chromosome/SampleId/g' -e 's/__/\//g' -e 's/--/|/g' -e 's/Avg depth/AverageDepth/g' -e 's/Cov /Coverage/g' -e 's/%//g' chromosomes.report")
 except:
     print("Error in assembly metrics step")
